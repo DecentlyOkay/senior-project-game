9e54a58a18f7a7a0b382b87e6d75cd7c6ef948f6
mwen <markhwen.1@gmail.com>
2021-02-04 20:19:35 -0600

    Jumping and Player Looking
    Added jumping and player facing direction is now dependent on if you're grounded or airborne. Shooting is still directed right at mouse point, need to fix that. Also first bug!—related to strafing into and obstacle while in the air, player will stutter in place.

--- Senior Project Game/Assets/Scripts/Gun.cs
+++ Senior Project Game/Assets/Scripts/Gun.cs
@@ -8,7 +8,6 @@ public class Gun : Weapon
 
     private PlayerController player;
 
-    // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<PlayerController>();
@@ -22,11 +21,20 @@ public class Gun : Weapon
             Shoot(mouseLoc);
         }
     }
+
+    //Idea for making aiming smarter. i.e. when aiming up ramps and when aiming at floor if cursor is really close to player
+    //Raycast from gun and check distance, if distance is small enough, then just shoot on player's y level/hit location + player's y scale
+    //else just shoot at hit location (where mouse is)
+    //Actually might just want to universally add the player y scale offset to hit location
+
     private void Shoot(RaycastHit hit)
     {
         Projectile projectile = Instantiate(projectilePrefab);
-        Vector3 pointAboveFloor = hit.point + new Vector3(0, this.transform.position.y, 0);
-        Vector3 direction = pointAboveFloor - this.transform.position;
+
+        //Vector3 pointAboveFloor = hit.point + new Vector3(0, this.transform.position.y, 0);
+        //Vector3 direction = pointAboveFloor - this.transform.position;
+        Vector3 direction = hit.point - this.transform.position;
+
         Ray shootRay = new Ray(this.transform.position, direction);
         Debug.DrawRay(shootRay.origin, shootRay.direction * 100.1f, Color.green, 1);
         Physics.IgnoreCollision(player.GetComponent<Collider>(), projectile.GetComponent<Collider>());
--- Senior Project Game/Assets/Scripts/PlayerController.cs
+++ Senior Project Game/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed = 20f;
     public float dashLength = 0.5f;
     public float dashCooldown = 0.5f;
-    public float jumpPower = 10f;
+    public float jumpHeight = 10f;
     public float gravity = -9.8f;
     public CharacterController controller;
     public LayerMask groundMask;
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     public float groundDistance = 0.4f;
 
     public Transform weaponHolder;
+    public Transform models;
 
     private Vector3 velocity;
     private bool isDashing = false;
@@ -44,20 +45,28 @@ public class PlayerController : MonoBehaviour
         //look at mouse cursor
         //probably will change to the model later
         //handle when grounded and in air separately?
-        RaycastHit hit;
-        Ray rayToFloor = Camera.main.ScreenPointToRay(
-            new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y));
-
-        if (Physics.Raycast(rayToFloor, out hit, 100, groundMask, QueryTriggerInteraction.Collide))
+        RaycastHit mouseLoc = RayCastToMouse();
+        if(mouseLoc.collider != null)
         {
-            transform.LookAt(hit.point + new Vector3(0, this.transform.position.y, 0));
+            Vector3 lookPoint = mouseLoc.point;
+            if(isGrounded)
+            {
+                lookPoint.y = transform.position.y;
+            }
+            models.transform.LookAt(lookPoint);
         }
 
-
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded)
+        {
+            Debug.Log("grounded" + Time.time);
+        }
+           
+
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = 0;
+            velocity.y = -2f;
         }
 
         //Character controller movement
@@ -71,11 +80,19 @@ public class PlayerController : MonoBehaviour
             if (dashStartTime + dashLength < Time.time)
                 isDashing = false;
 
-            //dash == speed boost
-            dashDirection = moveDirection;
+            //for if you want dash == speed boost
+            //dashDirection = moveDirection;
+
             dash = dashDirection * dashSpeed;
             
         }
+
+        //Might want to do some fancy code to make it where you can move within 90 degrees of a dash direction
+        //And movement in anything outside of that range will not register
+
+        //Actually might want to consider binding dash to right click, this will lose weapon functionality, i.e. scoping in
+        //but will allow you to dash in the mouse direction.
+
         controller.Move((move + dash) * Time.deltaTime);
 
         //Gravity
@@ -85,11 +102,18 @@ public class PlayerController : MonoBehaviour
     public RaycastHit RayCastToMouse()
     {
         RaycastHit hit;
-        //will probably want to change this when adding controller support
+        //will want to augment this when adding controller support
         Ray rayToFloor = Camera.main.ScreenPointToRay(
             new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y));
         Debug.DrawRay(rayToFloor.origin, rayToFloor.direction * 100.1f, Color.red, 1);
         Physics.Raycast(rayToFloor, out hit, 100.0f, groundMask, QueryTriggerInteraction.Collide);
+
+        //Make it so that when player is on lower ground and mouse clicks over higher ground, they y position of the hit
+        //will be at the player's feet's y position
+        //if(hit.collider != null)
+        //{
+        //    hit.point = new Vector3(hit.point.x, Mathf.Min(hit.point.y, transform.position.y), hit.point.z);
+        //}
         return hit;
     }
 
@@ -97,16 +121,26 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputValue input)
     {
         Vector2 inputVec = input.Get<Vector2>();
-        Debug.Log(inputVec);
         moveDirection = new Vector3(inputVec.x, 0, inputVec.y);
     }
     public void OnJump()
     {
-        Debug.Log("jump");
+        //Trying to make jumping feel better when walking down ramps. There will be greater distance leeway allowed when
+        //checking if you can perform a jump than in checking if you are grounded.
+
+        //Probably want to add coyote time + buffered jumps as well
+
+        if (/*isGrounded*/ Physics.CheckSphere(groundCheck.position, groundDistance+0.2f, groundMask))
+        {
+            velocity.y += jumpHeight;
+            Debug.Log("jump");
+            isGrounded = false;
+        }
+        
     }
     public void OnDash()
     {
-        //Dash currently on cooldown
+        //If dash is currently on cooldown, you will not dash
         if (nextDashTime > Time.time)
             return;
         
@@ -116,8 +150,10 @@ public class PlayerController : MonoBehaviour
         isDashing = true;
     }
 
+    //Will probably change the input to take in context in order to have automatic fire
     public void OnAttack()
     {
+        //Obviously need to augment this up when adding more weapons
         foreach (Transform weapon in weaponHolder)
         {
             Debug.Log(weapon);
